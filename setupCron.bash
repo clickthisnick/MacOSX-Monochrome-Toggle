1f6646f990a2bea2aa26e5e81fef011d0333b004
mycomputer <mycomputer@mycomputers-air.mynetworksettings.com>
2023-09-26 19:42:28 -0400

    fix close tabs script

--- setupCron.bash
+++ setupCron.bash
@@ -1,2 +1,5 @@
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+echo $SCRIPT_DIR
+
 (crontab -l | grep 'open /Users/mycomputer/Applications/Toggle\ Color\ Filters\ On\ Cron.app') || crontab -l | { cat; echo "* * * * * open /Users/mycomputer/Applications/Toggle\ Color\ Filters\ On\ Cron.app"; } | crontab -
-(crontab -l | grep 'cd /Users/mycomputer/dev/MacOSX-Monochrome-Toggle && /usr/bin/env osascript closeTabs.scpt') || crontab -l | { cat; echo "*/5 * * * * cd /Users/mycomputer/dev/MacOSX-Monochrome-Toggle && /usr/bin/env osascript closeTabs.scpt"; } | crontab -
+(crontab -l | grep 'cd $SCRIPT_DIR && /usr/bin/env osascript $SCRIPT_DIR/closeTabs.scpt') || crontab -l | { cat; echo "*/5 * * * * cd $SCRIPT_DIR && /usr/bin/env osascript $SCRIPT_DIR/closeTabs.scpt"; } | crontab -
